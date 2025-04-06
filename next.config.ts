a466868525bd74367c02dcbcfdc0ab5ec74dfe03
taha <gouyandeht@gmail.com>
2025-04-06 13:22:07 +0330

    ch

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/(.*)", // This applies to all routes
+        headers: [
+          {
+            key: "Permissions-Policy",
+            value: "fullscreen=*; usb=none; payment=self https://printer-test-ten.vercel.app",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
